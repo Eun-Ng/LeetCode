bbdbdef922c32003cdf9d0b40b7214c828690fbd
남궁은 <dhkdldms@gmail.com>
2022-05-31 00:33:21 +0900

    Time: 205 ms (15.66%), Space: 42.6 MB (60.99%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,13 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-
 var twoSum = function(nums, target) {
-    
-    for(let i = 0; i < nums.length - 1; i++) {
-        for(let j = i+1; j < nums.length; j++) {
-            if(target === nums[i] + nums[j]) return [i, j];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
         }
     }
-    
-}
+};
